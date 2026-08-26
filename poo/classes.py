23a89aa02cc3cd2b1186eb4ac9fb0a90f3349d17
J-Breno <joao.breno85@hotmail.com>
2026-08-26 03:44:17 -0300

    metodos estaticos

--- poo/classes.py
+++ poo/classes.py
@@ -1,8 +1,17 @@
 # o metodo __init__ ele sempre vai ser inicializado no começo da sua classe
+from datetime import datetime
+import pytz
 
 class Tv:
-    def __init__(self, cor, ligado, tamanho, canal):
-        self.cor = cor
+    cor = 'preta'
+
+    @staticmethod
+    def _data_hora(self):
+        fuso_horario = pytz.timezone('Brazil/East')
+        horario_BR = datetime.now(fuso_horario)
+        return horario_BR.strftime('%d/%m/%Y %H:%M:%S')
+
+    def __init__(self,  ligado, tamanho, canal):
         self.ligado = ligado
         self.tamanho = tamanho
         self.canal = canal
@@ -10,6 +19,9 @@ class Tv:
     def mudar_canal(self, novo_canal):
         self.canal = novo_canal
 
-tv_sala = Tv('Preta', False, 55, 'Netflix')
+    def _mostrar_preco(self): # metodo privado
+        print(2000)
+
+tv_sala = Tv( False, 55, 'Netflix')
 tv_sala.ligado = True
 tv_sala.mudar_canal("HBO Max")
