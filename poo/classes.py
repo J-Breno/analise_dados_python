f982a8db9dc8414ef951b20e0ce9fe1074b1ae0b
J-Breno <joao.breno85@hotmail.com>
2026-08-26 03:32:39 -0300

    Metodos e init

--- poo/classes.py
+++ poo/classes.py
@@ -1,10 +1,15 @@
 # o metodo __init__ ele sempre vai ser inicializado no começo da sua classe
 
 class Tv:
-    def __init__(self):
-        self.cor = "preta"
-        self.ligado = False
-        self.tamanho = 55
+    def __init__(self, cor, ligado, tamanho, canal):
+        self.cor = cor
+        self.ligado = ligado
+        self.tamanho = tamanho
+        self.canal = canal
 
-tv_sala = Tv()
+    def mudar_canal(self, novo_canal):
+        self.canal = novo_canal
+
+tv_sala = Tv('Preta', False, 55, 'Netflix')
 tv_sala.ligado = True
+tv_sala.mudar_canal("HBO Max")
